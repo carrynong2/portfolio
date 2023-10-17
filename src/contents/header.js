a0d9b539e6de626aa520b1ed475ac0cb04b55056
carrynong2 <carrynong2@gmail.com>
2023-10-17 11:22:01 +0700

    create header data for static content

---
+++ src/contents/header.js
@@ -0,0 +1,7 @@
+export const data = {
+  name: "Monthon Mukkun",
+  title: "Information Technology - Student",
+  caption: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis eligendi consequatur praesentium, dolores odit eveniet.",
+  btnText: "View Resume",
+  link: "#"
+}
--- src/sections/Header/index.jsx
+++ src/sections/Header/index.jsx
@@ -1,17 +1,18 @@
 import { faArrowDown } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
+import { data } from "../../contents/header"
 
 const Header = () => {
   return (
     <div className="flex flex-col gap-2">
-      <div className="text-3xl text-primaryTitle font-semibold">Monthon Mukkun</div>
-      <div className="text-primaryAccent font-semibold">Information Technology - Student</div>
-      <div className="text-sm w-5/6">Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis eligendi consequatur praesentium, dolores odit eveniet.</div>
+      <div className="text-3xl text-primaryTitle font-semibold">{data.name}</div>
+      <div className="text-primaryAccent font-semibold">{data.title}</div>
+      <div className="text-sm w-5/6">{data.caption}</div>
       <div className="mt-4">
-        <a href="#">
+        <a href={data.link}>
           <span className="rounded-md bg-primaryTitle text-gray-200 py-2 px-4">
-            View Resume
+            {data.btnText}
             <span className="rotate-90 inline-block ml-2 text-sm"><FontAwesomeIcon className="animate-bounce" icon={faArrowDown} /></span>
           </span>
         </a>
